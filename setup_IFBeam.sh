238c478ba1c6837be13ce615cd8a6c4ad889a780
Nuruzzaman <nur.jlab@gmail.com>
2019-03-06 07:21:47 -0600

    Setup the correct environment for the package.
    Setup the correct environment for the package.

---
+++ setup_IFBeam.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=/grid/fermiapp/minerva/software_releases/v10r8p7/minerva/EXTERNAL/EXTERNAL_v10r8p7/NuConDB/x86_64-slc6-gcc44-opt:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$NUCONDBROOT/$CMTCONFIG
